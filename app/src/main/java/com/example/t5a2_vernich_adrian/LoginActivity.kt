a7090fae909c8bb156bd71aec053ce2c3013c019
Adrián Vernich <vernicholtraadrian@gmail.com>
2024-11-01 13:23:35 +0100

    He copiado y pegado los archivos necesarios del t4a2_Vernich_Adrian

---
+++ app/src/main/java/com/example/t5a2_vernich_adrian/LoginActivity.kt
@@ -0,0 +1,25 @@
+package com.example.t4a2_vernich_adrian
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.t5a2_vernich_adrian.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val dni = intent.getStringExtra("DNI")
+        binding.textViewWelcome.text = "Bienvenido/a, $dni"
+
+        binding.btnSalir.setOnClickListener {
+            finish()
+        }
+    }
+}
