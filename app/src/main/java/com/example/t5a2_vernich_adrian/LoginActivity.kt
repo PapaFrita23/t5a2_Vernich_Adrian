4bd6516299b2260e80ca19cb4925091e6fae2d2b
Adrián Vernich <vernicholtraadrian@gmail.com>
2024-11-04 18:02:28 +0100

    04/11/2024 - Subida del proyecto (En proceso de terminarlo)

--- app/src/main/java/com/example/t5a2_vernich_adrian/LoginActivity.kt
+++ app/src/main/java/com/example/t5a2_vernich_adrian/LoginActivity.kt
@@ -27,7 +27,6 @@ class LoginActivity : AppCompatActivity() {
         binding.btnTransferencia.setOnClickListener {
             val intent = Intent(this, TransferActivity::class.java)
             startActivity(intent)
-            finish()
         }
     }
 }
--- app/src/main/java/com/example/t5a2_vernich_adrian/TransferActivity.kt
+++ app/src/main/java/com/example/t5a2_vernich_adrian/TransferActivity.kt
@@ -1,22 +1,42 @@
 package com.example.t5a2_vernich_adrian
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.t5a2_vernich_adrian.databinding.ActivityWelcomeBinding
-import com.example.t5a2_vernich_adrian.databinding.ActivityMainBinding
-import com.example.t5a2_vernich_adrian.databinding.ActivityLoginBinding
-import com.example.t5a2_vernich_adrian.databinding.ActivityTransferBinding
 
 class TransferActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityTransferBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_transfer)
 
-        binding = ActivityTransferBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val spCuentas: Spinner = findViewById(R.id.spinner)
+        val cuentas = resources.getStringArray(R.array.accounts)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, cuentas)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spCuentas.adapter = adapter
+        spCuentas.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val opcionSeleccionada = parent?.getItemAtPosition(position).toString()
+                Toast.makeText(this@TransferActivity, "Seleccionaste: $opcionSeleccionada", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+        }
     }
 }
